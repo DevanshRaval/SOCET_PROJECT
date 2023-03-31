d4842adcb51d3ae786d8bafd202b49b8bfdaa18c
ShivBhakt <ravaldevansh@gmail.com>
2023-03-31 23:55:55 +0530

    Home Page and Inbox Page Created

--- src/app/my-home/my-home.component.ts
+++ src/app/my-home/my-home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-home.component.css']
 })
 export class MyHomeComponent {
-
+  showModal : boolean =false;
+  todayTime : Date = new Date();
+  c : number = 0;  
+  currentTimeOut : any = 0;
+  constructor(){
+    setInterval(()=>{
+       let currentTime = new Date();
+       this.currentTimeOut = currentTime;
+    },1000)
+  }
 }
